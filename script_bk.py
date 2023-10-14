de7c7f11bff8cbaaf3198812803b807580a48e47
Henry De la Torre <86315728+sir-h3d@users.noreply.github.com>
2023-10-14 11:28:21 -0500

    Update script_bk.py

--- script_bk.py
+++ script_bk.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 df = pd.read_csv('Stores.csv', header=0, names=['Store ID', 'Store_Area', 'Items_Available', 'Daily_Customer_Count', 'Store_Sales'])
 
-cnx = mysql.connector.connect(user='admin', password='administrador',host='stores.cbvfkwq2aepn.us-east-1.rds.amazonaws.com', database='store_db')
+cnx = mysql.connector.connect(user='', password='',host='', database='')
 cursor = cnx.cursor()
 print("Conexion dada")
 for index, row in df.iterrows():
